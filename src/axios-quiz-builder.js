93ae49f4b28e8cc2633c3ec1cb3fc8a300e2d8b8
florianbar <florianbar01@gmail.com>
2020-10-08 09:08:42 +0200

    Updated firebase db

--- src/axios-quiz-builder.js
+++ src/axios-quiz-builder.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: "https://quiz-builder-react.firebaseio.com/"
+    baseURL: "https://quiz-builder-react-01.firebaseio.com/"
 });
 
 export default instance;
